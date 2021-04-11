ef367e9427347305e9f38245e90fa1981c6ec71e
lisellelogan <liselle.logan@yahoo.com>
2021-04-11 14:06:53 -0400

    commit tree 2e5403d57357069c58b361eaf20ebcb8c95ef485

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//function sums all the batter amounts in the array
+//use reduce to satisfy this function
+// function totalBatteries(arr){
+//     arr.reduce(function(total, element){
+//         total += element
+//     })
+//     return total
+// }
+
+const totalBatteries = batteryBatches.reduce(function(total, element){return total + element})
